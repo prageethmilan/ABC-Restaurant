d63f355d4d6cba9e8d1762e26415dcfcb2a4e70a
prageethmilan <prageethmilan1999@gmail.com>
2024-08-27 12:22:10 +0530

    Initial commit for initiate frontend and backend

---
+++ abc-restaurant-backend/src/test/java/com/abc/restaurant/OnlineReservationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineReservationSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
