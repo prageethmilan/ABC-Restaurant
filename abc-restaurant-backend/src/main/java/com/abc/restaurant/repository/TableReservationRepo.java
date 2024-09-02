3537aa0cdbfbc9a69ae6cafa4696c259de660835
prageethmilan <prageethmilan1999@gmail.com>
2024-09-02 10:14:27 +0530

    implement findByIdV2 method

--- abc-restaurant-backend/src/main/java/com/abc/restaurant/repository/TableReservationRepo.java
+++ abc-restaurant-backend/src/main/java/com/abc/restaurant/repository/TableReservationRepo.java
@@ -2,6 +2,7 @@ package com.abc.restaurant.repository;
 
 import com.abc.restaurant.entity.TableReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface TableReservationRepo extends JpaRepository<TableReservation, Long> {
     List<TableReservation> findTableReservationByCustomerId(Long id);
+
+    @Query(value = "SELECT tables_reservation.* FROM tables_reservation WHERE tables_reservation.id=?1", nativeQuery = true)
+    List<TableReservation> findByIdV2(Long id);
 }
