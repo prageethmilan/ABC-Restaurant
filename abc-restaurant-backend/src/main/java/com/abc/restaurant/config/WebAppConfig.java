47fcd44df2fc61f33dc979b81e4a0ec5343fcc7d
prageethmilan <prageethmilan1999@gmail.com>
2024-09-02 11:30:15 +0530

    Updated WebAppConfig.java

--- abc-restaurant-backend/src/main/java/com/abc/restaurant/config/WebAppConfig.java
+++ abc-restaurant-backend/src/main/java/com/abc/restaurant/config/WebAppConfig.java
@@ -3,11 +3,6 @@ package com.abc.restaurant.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-/**
- * @author : M-Prageeth
- * @created : 24/09/2022 - 6:55 PM
- **/
 @Configuration
 public class WebAppConfig {
 
