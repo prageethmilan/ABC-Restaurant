5484af1875657b80fa8e905d004a5109220990b6
prageethmilan <prageethmilan1999@gmail.com>
2024-09-05 09:03:47 +0530

    Update images

---
+++ abc-restaurant-frontend/src/views/adminPanel/meals/mealTable/mealTable.js
@@ -0,0 +1,259 @@
+// ** React Imports
+import { Fragment, useEffect, useState } from "react"
+
+// ** Third Party Components
+import ReactPaginate from 'react-paginate'
+import { ChevronDown, Edit } from 'react-feather'
+import { useTranslation } from 'react-i18next'
+import DataTable from 'react-data-table-component'
+
+// ** Reactstrap Imports
+import { Card, CardHeader, Badge, CardTitle, Input, Label, Row, Col } from 'reactstrap'
+import { getAllMeals } from "@src/services/meals"
+
+
+export const status = {
+  ACTIVE: { title: 'Active', color: 'light-success' },
+  INACTIVE: { title: 'Inactive', color: 'light-danger' }
+}
+
+const formatMealType = (mealType) => {
+  if (typeof mealType !== 'string') {
+    return mealType || '' // Return the original value if not a string, or an empty string if it's null/undefined
+  }
+  return mealType
+    .replace(/_/g, ' ')
+    .toLowerCase()
+    .replace(/\b\w/g, char => char.toUpperCase())
+}
+
+
+const MealTable = ({onEdit}) => {
+  // ** State
+  const [currentPage, setCurrentPage] = useState(0)
+  const [searchValue, setSearchValue] = useState('')
+  const [filteredData, setFilteredData] = useState([])
+  const [data, setData] = useState([])
+
+  const { t } = useTranslation()
+
+  const columns = [
+    {
+      name: 'Id',
+      sortable: true,
+      minWidth: '150px',
+      selector: row => row.id,
+      omit:true
+    },
+    {
+      name: 'Description',
+      sortable: true,
+      minWidth: '150px',
+      selector: row => row.description,
+      omit:true
+    },
+    {
+      name: 'Meal Name',
+      sortable: true,
+      minWidth: '220px',
+      selector: row => row.name
+    },
+    {
+      name: 'Price',
+      sortable: true,
+      minWidth: '80px',
+      selector: row => row.price
+    },
+    {
+      name: 'Discount',
+      sortable: true,
+      minWidth: '60px',
+      selector: row => row.discount
+    },
+    {
+      name: 'Meal Type',
+      sortable: true,
+      minWidth: '150px',
+      selector: row => formatMealType(row.mealType)
+    },
+    {
+      name: 'Main Category',
+      sortable: true,
+      minWidth: '150px',
+      selector: row => formatMealType(row.mainCategory)
+    },
+    {
+      name: 'Sub Category',
+      sortable: true,
+      minWidth: '150px',
+      selector: row => formatMealType(row.subCategory)
+    },
+    {
+      name: 'Rating',
+      sortable: true,
+      minWidth: '100px',
+      selector: row => formatMealType(row.rating)
+    },
+    {
+      name: 'Status',
+      sortable: true,
+      minWidth: '80px',
+      selector: row => row.status,
+      cell: row => {
+        return (
+          <Badge color={status[row.status].color} pill>
+            {status[row.status].title}
+          </Badge>
+        )
+      }
+    },
+    {
+      name: 'Actions',
+      allowOverflow: true,
+      minWidth: '90px',
+      cell: row => (
+        <div className='d-flex'>
+          <Edit
+            className={'text-info'}
+            size={18} onClick={() => onEdit(row.id)} />
+        </div>
+      )
+    }
+  ]
+
+  useEffect(() => {
+    getAllMelsProducts()
+  }, [])
+
+  const getAllMelsProducts = () => {
+    getAllMeals().then(async response => {
+      console.log(response)
+      const data = response.meals.map((item, index) => ({
+        ...item,
+        uniqueKey: `${item.id}-${index}` // Combine `id` and `index` to generate a unique key
+      }))
+      await setData(data)
+    })
+  }
+
+  // ** Function to handle pagination
+  const handlePagination = page => {
+    setCurrentPage(page.selected)
+  }
+
+  // ** Function to handle filter
+  const handleFilter = e => {
+    const value = e.target.value.toLowerCase()
+    let updatedData = []
+    setSearchValue(value)
+
+    const status = {
+      ACTIVE: { title: 'Active', color: 'light-success' },
+      INACTIVE: { title: 'Inactive', color: 'light-danger' }
+    }
+
+    if (value.length) {
+      updatedData = data.filter(item => {
+        const startsWith =
+          (item.name && item.name.toLowerCase().startsWith(value)) ||
+          (item.subCategory && item.subCategory.toLowerCase().startsWith(value)) ||
+          (item.mainCategory && item.mainCategory.toLowerCase().startsWith(value)) ||
+          (item.mealType && item.mealType.toLowerCase().startsWith(value)) ||
+          (status[item.status] && status[item.status].title.toLowerCase().startsWith(value))
+
+        const includes =
+          (item.name && item.name.toLowerCase().includes(value)) ||
+          (item.subCategory && item.subCategory.toLowerCase().includes(value)) ||
+          (item.mainCategory && item.mainCategory.toLowerCase().includes(value)) ||
+          (item.mealType && item.mealType.toLowerCase().includes(value)) ||
+          (status[item.status] && status[item.status].title.toLowerCase().includes(value))
+
+        return startsWith || includes
+      })
+      setFilteredData(updatedData)
+    }
+  }
+
+
+  // ** Pagination Previous Component
+  const Previous = () => {
+    return (
+      <Fragment>
+        <span className='align-middle d-none d-md-inline-block'>{t('Prev')}</span>
+      </Fragment>
+    )
+  }
+
+  // ** Pagination Next Component
+  const Next = () => {
+    return (
+      <Fragment>
+        <span className='align-middle d-none d-md-inline-block'>{t('Next')}</span>
+      </Fragment>
+    )
+  }
+
+  // ** Custom Pagination Component
+  const CustomPagination = () => (
+    <ReactPaginate
+      previousLabel={<Previous size={10} />}
+      nextLabel={<Next size={10} />}
+      forcePage={currentPage}
+      onPageChange={page => handlePagination(page)}
+      pageCount={Math.ceil((searchValue.length ? filteredData.length : data.length) / 7)}
+      breakLabel={'...'}
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={2}
+      activeClassName={'active'}
+      pageClassName={'page-item'}
+      nextLinkClassName={'page-link'}
+      nextClassName={'page-item next'}
+      previousClassName={'page-item prev'}
+      previousLinkClassName={'page-link'}
+      pageLinkClassName={'page-link'}
+      breakClassName='page-item'
+      breakLinkClassName='page-link'
+      containerClassName={'pagination react-paginate pagination-sm justify-content-end pe-1 mt-1'}
+    />
+  )
+
+  return (
+    <Card>
+      <CardHeader className='border-bottom'>
+        <CardTitle tag='h4'>Meal Products</CardTitle>
+      </CardHeader>
+      <Row className='justify-content-start mx-0'>
+        <Col className='d-flex align-items-center justify-content-end mt-1' md='4' sm='12'>
+          <Label className='me-1' for='search-input-1'>
+            {t('Search')}
+          </Label>
+          <Input
+            className='dataTable-filter mb-50'
+            type='text'
+            bsSize='sm'
+            id='search-input-1'
+            value={searchValue}
+            onChange={handleFilter}
+          />
+        </Col>
+      </Row>
+      <div className='react-dataTable'>
+        <DataTable
+          noHeader
+          pagination
+          selectableRowsNoSelectAll
+          columns={columns}
+          className='react-dataTable'
+          paginationPerPage={7}
+          sortIcon={<ChevronDown size={10} />}
+          paginationDefaultPage={currentPage + 1}
+          paginationComponent={CustomPagination}
+          data={searchValue.length ? filteredData : data}
+          keyField="uniqueKey"
+        />
+      </div>
+    </Card>
+  )
+}
+
+export default MealTable
---
+++ abc-restaurant-frontend/src/views/adminPanel/meals/meals.js
@@ -0,0 +1,443 @@
+import React, { useEffect, useState } from "react"
+import SpinnerComponent from "@components/spinner/Fallback-spinner"
+import { Button, Card, CardBody, Col, Form, Input, Label, Modal, ModalBody, ModalHeader, Row } from "reactstrap"
+import Breadcrumbs from "@components/breadcrumbs"
+import { UserPlus } from "react-feather"
+import MealTable from "@src/views/adminPanel/meals/mealTable/mealTable"
+import { Assets } from "@src/assets/images"
+import { USER_LOGIN_DETAILS } from "@src/router/RouteConstant"
+import Select from "react-select"
+import { validateMealsDetails } from "@src/utility/validation"
+import toast from "react-hot-toast"
+import { addNewMeal, findMealById } from "@src/services/meals"
+import { getAllRestaurantsIds } from "@src/services/restaurants"
+
+const statusOptions = [
+  { value: "ACTIVE", label: "Active" },
+  { value: "INACTIVE", label: "Inactive" }
+]
+
+const mainCategoryOptions = [
+  { value: "LUNCH", label: "Lunch" },
+  { value: "DINNER", label: "Dinner" },
+  { value: "BREAKFAST", label: "Breakfast" },
+  { value: "TEA_TIME", label: "Tea-Time" }
+]
+
+
+const subCategoryOptions = [
+  { value: "STARTERS_SOUP", label: "Starters & Soup" },
+  { value: "SALAD", label: "Salad" },
+  { value: "CHICKEN", label: "Chicken" },
+  { value: "BIRIYANI", label: "Biryani" },
+  { value: "BBQ", label: "BBQ" },
+  { value: "INTERNATIONAL", label: "International" },
+  { value: "SANDWICH", label: "Sandwich" },
+  { value: "DESSERT", label: "Dessert" },
+  { value: "EXTRA", label: "Extra" },
+  { value: "FRESH_JUICE", label: "Fresh Juice" },
+  { value: "TEA_COFFEE", label: "Tea & Coffee" }
+]
+
+const mealTypeOptions = [
+  { value: "SRI_LANKAN", label: "Sri Lankan" },
+  { value: "INDIAN", label: "Indian" },
+  { value: "FRENCH", label: "French" },
+  { value: "ITALIAN", label: "Italian" }
+]
+
+const ratingsOptions = [
+  { value: 1, label:'1 Star'},
+  { value: 2, label:'2 Star' },
+  { value: 3, label:'3 Star'},
+  { value: 4, label:'4 Star'},
+  { value: 5, label:'5 Star'}
+]
+function Meals() {
+  const [show, setShow] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [loggedUser, setLoggedUser] = useState(null)
+  const [isEdit, setIsEdit] = useState(false)
+  const [featuredImg, setFeaturedImg] = useState(Assets.emptyImg)
+  const [restaurantsOptions, setRestaurantsOptions] = useState([])
+  const [form, setForm] = useState({
+    id:0,
+    name: "",
+    restaurantId: null,
+    mainCategory: null,
+    subCategory: null,
+    mealType: null,
+    price: 0,
+    discount: 0,
+    status: null,
+    rating: null,
+    description: "",
+    img: null
+  })
+
+  useEffect(() => {
+    fetchAllRestaurantsIds()
+  }, [])
+
+  const fetchAllRestaurantsIds = () => {
+    getAllRestaurantsIds().then(async response => {
+
+      const data = response.data.map((item, index) => ({
+        ...item,
+        uniqueKey: `${item.id}-${index}`
+      }))
+      await setRestaurantsOptions(data)
+    })
+  }
+
+  useEffect(() => {
+    // Fetch the logged-in user details from local storage
+    const storedUserDetails = localStorage.getItem(USER_LOGIN_DETAILS)
+    const user = storedUserDetails ? JSON.parse(storedUserDetails) : null
+    setLoggedUser(user)
+
+  }, [])
+
+  const onChangeImg = (e) => {
+    const reader = new FileReader()
+    const files = e.target.files
+    if (files.length > 0) {
+      reader.onload = function() {
+        setFeaturedImg(reader.result)
+      }
+      reader.readAsDataURL(files[0])
+      setForm((prev) => ({
+        ...prev,
+        img: files[0] // Set the file in the form state
+      }))
+    }
+  }
+
+  const onTextChange = (event) => {
+    const { name, value } = event.target
+    setForm(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const onSelectChange = (selectedOption, actionMeta) => {
+    setForm(prev => ({
+      ...prev,
+      [actionMeta.name]: selectedOption
+    }))
+  }
+
+  const clearForm = () => {
+    setForm({
+      id:0,
+      name: "",
+      restaurantId: null,
+      mainCategory: null,
+      subCategory: null,
+      mealType: null,
+      price: 0,
+      discount: 0,
+      status: null,
+      rating: null,
+      description: "",
+      img: null
+    })
+    setFeaturedImg(Assets.emptyImg)
+  }
+
+  const createNewMeal = (form) => ({
+    id:form.id ?? null,
+    restaurantId: form.restaurantId?.value ?? null,
+    name: form.name ?? null,
+    mainCategory: form.mainCategory?.value ?? null,
+    subCategory: form.subCategory?.value ?? null,
+    mealType: form.mealType?.value ?? null,
+    price: form.price ?? 0,
+    discount: form.discount ?? 0,
+    status: form.status?.value ?? null,
+    rating: form.rating?.value ?? null,
+    description: form.description ?? "",
+    img: form.img ?? null
+  })
+
+  const apiHandler = () => {
+    if (validateMealsDetails(form, isEdit)) {
+      setLoading(true)
+
+      addNewMeal(createNewMeal(form))
+        .then((response) => {
+          if (response.success) {
+            toast.success(response.message)
+            clearForm()
+            setShow(false)
+          } else {
+            toast.error(response.message)
+          }
+        })
+        .catch((error) => {
+          console.error("API Request Error:", error.message)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+  }
+
+  const handleAddMealClick = () => {
+    setIsEdit(false)  // Ensure it's in save mode
+    setShow(true)
+    clearForm()
+  }
+
+  const handleEditClick = (byId) => {
+    setShow(true)
+    setLoading(true)
+    setIsEdit(true) // Set to edit mode
+    findMealById(byId)
+      .then(response => {
+        if (response.success) {
+          const res = response.data
+    setForm({
+      id:res.id,
+      restaurantId: restaurantsOptions.find(option => option.value === res.restaurantId),
+      name:res.name,
+      mainCategory: mainCategoryOptions.find(option => option.value === res.mainCategory),
+      subCategory: subCategoryOptions.find(option => option.value === res.subCategory),
+      mealType: mealTypeOptions.find(option => option.value === res.mealType),
+      price:res.price,
+      discount: res.discount,
+      status: statusOptions.find(option => option.value === res.status),
+      rating: ratingsOptions.find(option => option.value === res.rating),
+      description:res.description,
+      img: null
+    })
+          setFeaturedImg(res.img)
+        } else {
+          toast.error("Something went wrong !!")
+          setFeaturedImg(Assets.emptyImg)
+        }
+      }).finally(
+      setLoading(false)
+    )
+
+  }
+
+
+  const mealModal = (
+    <Modal isOpen={show} toggle={() => {
+      setShow(!show)
+      clearForm()
+    }} className="modal-dialog-centered modal-lg modal-danger">
+      <ModalHeader className={'text-info'} toggle={() => {
+        clearForm()
+        setShow(!show)
+      }}> {isEdit ? "Update Meal Product" : "Create New Meal"}</ModalHeader>
+      <ModalBody>
+        <Card>
+          <CardBody className="py-2 my-25">
+            <Form className="mt-2 pt-50">
+              <Row>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="meal-name">
+                    Meal Name
+                  </Label>
+                  <Input
+                    id="meal-name"
+                    type={"text"}
+                    name="name"
+                    value={form.name}
+                    placeholder="Enter meal name"
+                    onChange={onTextChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="restaurant-id">
+                    Restaurant
+                  </Label>
+                  <Select
+                    id="restaurant-id"
+                    name="restaurantId"
+                    options={restaurantsOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.restaurantId}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="main-category">
+                    Main Category
+                  </Label>
+                  <Select
+                    id="main-category"
+                    name="mainCategory"
+                    options={mainCategoryOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.mainCategory}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="sub-category">
+                    Sub Category
+                  </Label>
+                  <Select
+                    id="sub-category"
+                    name="subCategory"
+                    options={subCategoryOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.subCategory}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="meal-type">
+                    Meal Type
+                  </Label>
+                  <Select
+                    id="meal-type"
+                    name="mealType"
+                    options={mealTypeOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.mealType}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="status">
+                    Status
+                  </Label>
+                  <Select
+                    id="status"
+                    name="status"
+                    options={statusOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.status}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="rating">
+                    Rating
+                  </Label>
+                  <Select
+                    id="rating"
+                    name="rating"
+                    options={ratingsOptions}
+                    className="react-select"
+                    classNamePrefix="select"
+                    value={form.rating}
+                    onChange={onSelectChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="price">
+                    Price
+                  </Label>
+                  <Input
+                    type="number"
+                    id="price"
+                    name="price"
+                    value={form.price}
+                    placeholder="Enter price"
+                    onChange={onTextChange}
+                  />
+                </Col>
+                <Col sm="6" className="mb-1">
+                  <Label className="form-label" for="discount">
+                    Discount
+                  </Label>
+                  <Input
+                    type="number"
+                    id="discount"
+                    name="discount"
+                    value={form.discount}
+                    placeholder="Enter discount"
+                    onChange={onTextChange}
+                  />
+                </Col>
+                <Col sm="12" className="mb-1">
+                  <Label className="form-label" for="description">
+                    Description
+                  </Label>
+                  <Input
+                    type="textarea"
+                    id="description"
+                    name="description"
+                    value={form.description}
+                    placeholder="Enter description"
+                    onChange={onTextChange}
+                  />
+                </Col>
+                <Col sm="12" className="mb-1">
+                  <Label className="form-label" for="meal-image">
+                    Meal Image
+                  </Label>
+                  <Input
+                    type="file"
+                    id="meal-image"
+                    name="img"
+                    onChange={onChangeImg}
+                  />
+                  <img className="rounded mt-1" src={featuredImg} alt="Meal" height="200" width="250" />
+                </Col>
+              </Row>
+              <Col className="mt-2" sm="12">
+                <Button type="button"
+                        className="me-1"
+                        color="primary"
+                        onClick={apiHandler}
+                >
+                  Save Changes
+                </Button>
+                <Button color="secondary"
+                        outline
+                        type={"button"}
+                        onClick={clearForm}
+                >
+                  Discard
+                </Button>
+              </Col>
+            </Form>
+          </CardBody>
+        </Card>
+      </ModalBody>
+    </Modal>
+  )
+
+  return (
+    <>
+      {loading === true ? (
+        <SpinnerComponent />
+      ) : (
+        <div>
+          <Row className="d-flex ">
+            <Col md={6} cla>
+              <Breadcrumbs title='Manage Meals' data={[{ title: 'Admin' }, { title: 'meals' }]} />
+            </Col>
+            <Col md={6} className={'d-flex justify-content-end align-items-center'}>
+              <Button className='width-30-per mt-1 me-2'
+                      color={'warning'}
+                      onClick={handleAddMealClick}
+              >
+
+                <UserPlus size={18}/> <span > Add New Meals</span>
+              </Button>
+            </Col>
+          </Row>
+
+          <Row className={'mt-3'}>
+            <MealTable onEdit={handleEditClick} /> {/* Pass handleEditClick */}
+            {mealModal} {/*model calling place*/}
+          </Row>
+        </div>)}
+    </>
+  )
+}
+
+export default Meals
