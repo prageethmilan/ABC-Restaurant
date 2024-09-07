7707729b2642e5dc662c255df3251271e4e089b4
prageethmilan <prageethmilan1999@gmail.com>
2024-09-07 21:14:09 +0530

    JUnit testing

--- abc-restaurant-backend/src/test/java/com/abc/restaurant/ApplicationInitializerTests.java
+++ abc-restaurant-backend/src/test/java/com/abc/restaurant/ApplicationInitializerTests.java
@@ -1,9 +1,11 @@
 package com.abc.restaurant;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationInitializerTests {
 
 	@Test
